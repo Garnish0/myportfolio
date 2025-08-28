58c1e3963effd82474e46712c2c8d757d53fa858
Mehdi <massrali@hexmount.io>
2025-08-28 14:28:39 +0300

    Update padding in testimonials and projects sections for better responsiveness
    - Changed padding from `py-12` to `py-2 sm:py-12` to improve layout on smaller screens.
    - Ensures consistent spacing across different screen sizes for a better user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,7 +179,7 @@ export default function Page() {
       </section>
 
       <section id="testimonials">
-        <div className="space-y-12 w-full py-12">
+        <div className="space-y-12 w-full py-2 sm:py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 5.75}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -263,7 +263,7 @@ export default function Page() {
         </div>
       </section>
       <section id="projects">
-        <div className="space-y-12 w-full py-12">
+        <div className="space-y-12 w-full py-2 sm:py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 8.3}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
